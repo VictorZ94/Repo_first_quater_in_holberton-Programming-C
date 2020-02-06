19332d08ce38b303e502d9d6b202909ca911f935
victorZ94 <zrvictor@outlook.com>
2020-02-06 22:26:07 +0000

    print alphabetos

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lalp;
+
+	for (lalp = 'a'; lalp <= 'z'; lalp++)
+	{
+	putchar(lalp);
+	}
+	for (lalp = 'A'; lalp <= 'Z'; lalp++)
+	{
+	putchar(lalp)
+	}
+return (0);
+}
