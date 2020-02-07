e9212d92b819b6994cb7feb6e5c7510d20e3b6e5
victorZ94 <zrvictor@outlook.com>
2020-02-07 01:56:39 +0000

    print alphabet reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int alpr;
+
+	for (alpr = 'z'; alpr >= 'a'; alpr--)
+	{
+	putchar(alpr);
+	}
+	putchar('\n');
+return (0);
+}
