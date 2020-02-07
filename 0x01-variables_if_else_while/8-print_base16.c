9add493dc1b0b5a9c2a0450b40cdacff82795672
victorZ94 <zrvictor@outlook.com>
2020-02-07 02:15:09 +0000

    print hexadecimal number

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 48; hex <= 57; hex++)
+	{
+	putchar(hex);
+	}	
+		for (hex = 65; hex <= 70; hex++)
+		{
+		putchar(hex);
+		}
+		putchar('\n');
+	return (0);
+}
