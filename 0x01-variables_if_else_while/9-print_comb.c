24b326f3d51d4036c709075f34e0ce0e544d605d
victorZ94 <zrvictor@outlook.com>
2020-02-07 02:48:25 +0000

    combinaciones

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int comb;
+
+	for (comb = 48; comb <= 57; comb++)
+	{
+	putchar(comb);
+	if (comb != 57)
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+return (0);
+}
