149770a116acf2bf787c120ef8e1c720131acfc9
victorZ94 <zrvictor@outlook.com>
2020-02-10 22:56:33 +0000

    to print own _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**to print with putchar
+ *
+ *
+ */
+
+int main(void)
+{
+	int i;
+	char c[] = "Holberton";
+
+
+	for (i = 0; i <= 9; i++)
+	{
+	_putchar(c[i]);
+	}
+	_putchar('\n');
+	
+return(0);
+}
