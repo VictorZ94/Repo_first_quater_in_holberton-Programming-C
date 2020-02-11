6550697668f81792749fe8258b364cc33d42195e
victorZ94 <zrvictor@outlook.com>
2020-02-11 16:01:12 +0000

    alphabet print

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
+ * print_alphabet - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet(void) /*nothing it all*/
 {
 	int i;
 
