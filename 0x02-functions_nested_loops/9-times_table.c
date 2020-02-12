ff47de2c2c648440f9549e6925444990753d98e9
victorZ94 <zrvictor@outlook.com>
2020-02-12 21:50:17 +0000

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,7 +20,7 @@ void times_table(void)
 		_putchar (z / 10 + '0');
 		_putchar (z % 10 + '0');
 		}
-		else if ((n != 0) && (n != 9))
+		else if ((n != 0) && (n <= 9))
 		{
 		_putchar (' ');
 		_putchar (z + '0');
