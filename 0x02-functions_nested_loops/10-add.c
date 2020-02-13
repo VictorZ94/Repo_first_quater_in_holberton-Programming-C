243b0d99111645bb1934105eba1069fa24119a6d
victorZ94 <zrvictor@outlook.com>
2020-02-13 04:08:21 +0000

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,14 +1,13 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * add - check the code for Holberton School students.
+ *@n1: - variable
+ *@n2: - variable
  * Return: Always 0.
  */
 
 int add(int n1, int n2)
 {
-
-	return( n1 + n2);
-
+	return (n1 + n2);
 }
