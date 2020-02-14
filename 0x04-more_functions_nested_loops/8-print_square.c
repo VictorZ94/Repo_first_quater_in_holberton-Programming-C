45a37a23c87b5bac4803799c5bc86aaeed32a73c
victorZ94 <zrvictor@outlook.com>
2020-02-14 01:31:24 +0000

    used squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_square - check the code for Holberton School students.
+ * @size: - treated variable
+ * Return: Always 0.
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	for (y = 0; y < size; y++)
+	{
+		for (x = 0; x < size; x++)
+		{
+		_putchar ('#');
+		}
+	_putchar ('\n');
+	}
+}
