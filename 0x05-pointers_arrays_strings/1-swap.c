70f6419e7b4983097a6b11a64c8d4a1cd48c89f3
victorZ94 <zrvictor@outlook.com>
2020-02-18 18:01:34 +0000

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: - Variable
+ * @b: variable
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	*b = 98;
+	*a = 42;
+
+}
