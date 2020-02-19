d2647007e59e208278b1712dbff0016171597e37
victorZ94 <zrvictor@outlook.com>
2020-02-19 22:42:53 +0000

    fuck rev string it gave headache to me

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * rev_string - check the code for Holberton School students.
+ * @s: - varible
  * Return: Always 0.
  */
 
 void rev_string(char *s)
 {
-	int i,j,c;
+	int i, j, c;
 
 	while (s[i] != '\0')
 	{
