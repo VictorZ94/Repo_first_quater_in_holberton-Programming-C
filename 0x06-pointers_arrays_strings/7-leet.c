78c57bb8e4adbc45bf050ccd9bb3ae06f9e642f4
victorZ94 <zrvictor@outlook.com>
2020-02-22 03:59:24 +0000

    leet m

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -10,14 +10,14 @@
 char *leet(char *s)
 {
 	char s1[] = "AaEeOoTtLl";
-	char s2[] = "52525151484855554949";
+	char s2[] = {'4','4','3','3','0','0','7','7','1','1'};
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s1[j] != '\0'; j++)
 		{
-			if (s1[i] == s[j])
+			if (s[i] == s1[j])
 			{
 				s[i] = s2[j];
 			/*	break;*/
