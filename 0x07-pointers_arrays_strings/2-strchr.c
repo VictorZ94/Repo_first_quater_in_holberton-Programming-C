8c5521b7a05ae68800a174eae5c1e03ada9504f1
victorZ94 <zrvictor@outlook.com>
2020-02-24 20:40:07 +0000

    string character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,14 +10,18 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s != c)
+	while (*s != '\0')
 	{
 		if (*s == c)
+		{
 			return (s);
+		}
 		s++;
 	}
-	if (*s == c)
+	if (*s == '\0')
+	{
 		return (s);
+	}
 
 	return ('\0');
 }
