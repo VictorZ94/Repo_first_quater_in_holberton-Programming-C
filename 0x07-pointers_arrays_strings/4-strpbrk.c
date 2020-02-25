4479df005f13f259dcfefba950110734ad721a70
victorZ94 <zrvictor@outlook.com>
2020-02-25 23:41:55 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,13 +7,13 @@
  * Return: Always 0.
  */
 
-char *_strstr(char *haystack, char *needle)
+char *_strpbrk(char *s, char *accept)
 {
 	const char *sc1,*sc2;
 
-	for (sc1 = haystack; *sc1 != '\0'; ++sc1)
+	for (sc1 = s; *sc1 != '\0'; ++sc1)
 	{
-		for (sc2 = needle; *sc2 != '\0'; ++sc2)
+		for (sc2 = accept; *sc2 != '\0'; ++sc2)
 		{
 		if (*sc1 == *sc2)
 		return (char *) sc1;
