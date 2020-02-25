80c77f3dbe88e81e68e91c7ee932be6ea43d0dc3
victorZ94 <zrvictor@outlook.com>
2020-02-25 23:48:20 +0000

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,22 +2,23 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * _strpbrk - check the code for Holberton School students.
+ * @s: - varibale
+ * @accept: - variable
  * Return: Always 0.
  */
 
 char *_strpbrk(char *s, char *accept)
 {
-	const char *sc1,*sc2;
+	const char *sc1, *sc2;
 
 	for (sc1 = s; *sc1 != '\0'; ++sc1)
 	{
 		for (sc2 = accept; *sc2 != '\0'; ++sc2)
 		{
 		if (*sc1 == *sc2)
-		return (char *) sc1;
+		return ((char *) sc1);
 		}
 	}
-  return NULL;
+return ('\0');
 }
