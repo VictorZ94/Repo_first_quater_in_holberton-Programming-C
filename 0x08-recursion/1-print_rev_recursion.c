7eccbdecc001ddb615f9902b7a5641bd74093eed
victorZ94 <zrvictor@outlook.com>
2020-02-26 23:27:26 +0000

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - check the code for Holberton School students.
+ * @s: variable type char
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar (*s);
+}
