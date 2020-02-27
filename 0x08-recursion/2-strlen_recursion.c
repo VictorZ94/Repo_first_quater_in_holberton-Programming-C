323b49400a570e04e3ec71836d5fcccfe4cbf399
victorZ94 <zrvictor@outlook.com>
2020-02-27 04:43:52 +0000

    string lenght

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: - Variabl
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return (1 + _strlen_recursion(s + 1));
+}
