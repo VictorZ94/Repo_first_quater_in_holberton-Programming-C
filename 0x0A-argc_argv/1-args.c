867f3a6a659560af8e75fc51fdd2494b698e3f8a
victorZ94 <zrvictor@outlook.com>
2020-03-01 19:39:37 +0000

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - echo command-line arguments; 1st version
+ * main - check the code for Holberton School students.
+ * @argc: for argument count
+ * @argv: for argument vector
+ * Return: Always 0.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+
+
+	printf("%d\n", argc - 1);
+
+return (0);
+}
