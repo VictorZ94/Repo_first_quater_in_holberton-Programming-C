4693ed64bd67fa25fd9a0b538734a3bbaac09a43
victorZ94 <zrvictor@outlook.com>
2020-03-04 03:24:45 +0000

    concaten string

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,8 +14,11 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	int size, j, z;
 
-	if (s1 == NULL || s2 == NULL)
-		return ('\0');
+	if (s1 == NULL)
+		s1 = " ";
+
+	if (s2 == NULL)
+		s2 = " ";
 
 	while (s1[i] != '\0')
 	{
