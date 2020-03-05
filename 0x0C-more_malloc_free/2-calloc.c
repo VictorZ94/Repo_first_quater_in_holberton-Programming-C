af3298ee1d41e5eb6e055db5cd44943f156c1b51
victorZ94 <zrvictor@outlook.com>
2020-03-05 22:12:32 +0000

    contigous allocates

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,13 +19,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
 
-
 	s = malloc(size * nmemb);
 
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
 		s[i] = 0;
 	}
