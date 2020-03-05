0a8b14bf4a08c9b19d023611bd158a0ddd37ec09
victorZ94 <zrvictor@outlook.com>
2020-03-05 20:57:03 +0000

    string concat in n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - check the code for Holberton School students.
+ * @s1: pointer a char
+ * @s2: pointer a char
+ * Return: pointer a char.
+ * @n: return
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0;
+	unsigned int j = 0;
+	int size;
+	unsigned int size2;
+	char *s;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[i] != '\0')
+	{
+	i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+	j++;
+	}
+
+	size = (i + j);
+
+	s = malloc(sizeof(char) * size + 1);
+	if (s == NULL)
+		return (NULL);
+
+	size = 0;
+	while (size < i)
+	{
+		s[size] = s1[size];
+		size++;
+	}
+
+	size2 = 0;
+	while (size2 < n && size2 < j)
+	{
+		s[size] = s2[size2];
+		size++;
+		size2++;
+	}
+s[size] = '\0';
+return (s);
+}
