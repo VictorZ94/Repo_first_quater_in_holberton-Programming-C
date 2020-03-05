4d069d929ab912c51a094868ede08a0e45349520
victorZ94 <zrvictor@outlook.com>
2020-03-05 22:05:31 +0000

    contigous allocate

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *s;
+
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
+
+
+	s = malloc(size * nmemb);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		s[i] = 0;
+	}
+
+return (s);
+}
