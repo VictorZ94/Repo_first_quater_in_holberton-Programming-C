0a3d434fc9f0fabb4bd8bf35db12ea52b73f3046
victorZ94 <zrvictor@outlook.com>
2020-03-10 03:41:59 +0000

    print dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - check the code for Holberton School students.
+ * @d: first member
+ * Return: Always 0.
+ */
+
+void print_dog(struct dog *d)
+{
+	struct dog;
+
+	if (d == NULL)
+		return;
+
+	if (d == NULL)
+		printf("(nil) \n");
+
+	if ((*d).name == NULL)
+		printf("Name: (nil)");
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
