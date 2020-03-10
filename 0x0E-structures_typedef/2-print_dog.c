d6ac46435d910fc1ae45b36f037c0cc78c13d9c2
victorZ94 <zrvictor@outlook.com>
2020-03-10 03:50:34 +0000

    print dog order if matter

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,14 +11,14 @@ void print_dog(struct dog *d)
 {
 	struct dog;
 
-	if (d == NULL)
-		return;
+	if ((*d).name == NULL)
+		printf("Name: (nil) \n");
 
 	if ((*d).owner == NULL)
-		printf("Owner: (nil) \n");
+		printf("Owner: (nil)\n");
 
-	if ((*d).name == NULL)
-		printf("Name: (nil)\n");
+	if (d == NULL)
+		return;
 
 	printf("Name: %s\n", d->name);
 	printf("Age: %.1f\n", d->age);
