b74c0c1f56c1fcb18e9a0d76634ec4969c0dd2a9
victorZ94 <zrvictor@outlook.com>
2020-03-10 04:29:06 +0000

    initialize variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+/**
+ * init_dog - Descripción breve
+ * struct dog - structure
+ * @name: Primer miembro
+ * @age: Segundo miembro
+ * @owner: 3th member
+ * @d: struct
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	(*d).owner = owner;
+}
