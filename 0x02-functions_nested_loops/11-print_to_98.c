4c31dcdf086d7994842558daa8601e578eae51ed
victorZ94 <zrvictor@outlook.com>
2020-03-10 03:43:08 +0000

    this to up all struct prototype

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -10,34 +10,6 @@ void print_to_98(int n)
 {
 	int r, s;
 
-	if (n == 0)
-	{
-		for ( ; n <= 98; n++)
-		{
-			if (n > 9)
-			{
-			_putchar (n / 10 + '0');
-			_putchar (n % 10 + '0');
-			
-				if (n != 98)
-				{
-				_putchar (',');
-				_putchar (' ');
-				}
-			}
-			else 
-			{
-			_putchar (n + '0');
-			_putchar (',');
-			_putchar (' ');
-			}
-		}
-	}
-	else if ( n == 98)
-	{
-	_putchar (n / 10 + '0');
-	_putchar (n % 10 + '0');
-	}
 
 	else if (n == 111)
 	{
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,6 +2,6 @@
 
 #define ABS_VALUE
 
-#define ABS(x) ((x) > 0 ? (x) : (x) * -1)
+#define ABS(x) ((x) < 0 ? (x) : ((x) * -1))
 
 #endif /* absolute value */
