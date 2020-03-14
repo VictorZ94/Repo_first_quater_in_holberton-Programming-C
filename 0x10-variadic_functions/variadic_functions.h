fa18cc93b8a96c240b944895da8fde5fc7d8e200
victorZ94 <zrvictor@outlook.com>
2020-03-14 03:23:39 +0000

    variadic functions

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,11 +1,9 @@
 #ifndef VAR_FUNCTION_H
 #define VAR_FUNCTION_H
 
-
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
-
-	
-
-
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
