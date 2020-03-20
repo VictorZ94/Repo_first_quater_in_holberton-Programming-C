09c645f88a7489203a0ffd5d05402ef3f3674a47
victorZ94 <zrvictor@outlook.com>
2020-03-20 04:55:37 +0000

    free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list - function that
+ * frees a list_t list.
+ * @head: pointer to the head of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		/**
+		*freeing the content
+		*/
+		free(temp->str);
+		/**
+		*freeing the memory allocated with malloc
+		*/
+		free(temp);
+	}
+	/**
+	 * freees the las memory allocation
+	 */
+	free(head);
+}
