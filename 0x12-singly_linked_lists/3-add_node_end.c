dc0c747d79e70dfbecbf83abfa8d668c6cb969c8
victorZ94 <zrvictor@outlook.com>
2020-03-20 04:50:07 +0000

    add node end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node_end - check the code for Holberton School students.
+ * @head: hdosad
+ * @str: jdjdfj
+ * Return: Always 0.
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+
+	list_t *newNodo;
+	list_t *tmp;
+
+	int newlen;
+
+	for (newlen = 0; str[newlen]; newlen++)
+	{
+	}
+
+	newNodo = malloc(sizeof(list_t));
+
+	newNodo->str = strdup(str);
+	newNodo->len = newlen;
+	newNodo->next = NULL;
+
+	if (*head == NULL)
+	{
+		newNodo->next = *head;
+		*head = newNodo;
+	}
+	else
+	{
+		tmp = *head;
+
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = newNodo;
+	}
+
+return (newNodo);
+}
