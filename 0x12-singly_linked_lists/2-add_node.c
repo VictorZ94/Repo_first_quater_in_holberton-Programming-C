29d568996688fa09de66261156267426ee8721bf
victorZ94 <zrvictor@outlook.com>
2020-03-20 02:06:51 +0000

    duplicate new string in memory allocation

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,51 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - print contain list
+ * @str: the string added
+ * @head: the first string in list
+ * Return: pointer to function
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new;
+	int i, newlen;
+	char *duplicate;
+
+	/*newlen = strdup(str);*/
+	for (newlen = 0; str[newlen]; newlen++)
+	{
+	}
+
+	new = *head;
+
+	new = malloc(sizeof(list_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	duplicate = malloc(sizeof(char) * newlen + 1);
+
+	if (str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		duplicate[i] = str[i];
+	}
+
+	new->str = duplicate;
+	new->len = newlen;
+	new->next = *head;
+	*head = new;
+
+return (new);
+}
