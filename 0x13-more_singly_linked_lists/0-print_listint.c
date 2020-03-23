4d45679279ccda27ced74b060c27ccc420990538
victorZ94 <zrvictor@outlook.com>
2020-03-23 05:57:44 +0000

    prints element within liked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,19 +1,17 @@
 #include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
+ * print_listint - singly linked list
+ * @h: parameter pointer to int n
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Return: iterator
  */
 
 size_t print_listint(const listint_t *h)
 {
 
 	int i = 0;
-	
+
 	while (h !=  '\0')
 	{
 
