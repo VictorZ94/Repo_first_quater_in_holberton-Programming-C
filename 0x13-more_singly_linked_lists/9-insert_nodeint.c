4bb04e2dc8a6754551d1299826458616d10e154a
victorZ94 <zrvictor@outlook.com>
2020-03-25 00:37:17 +0000

    inser new nodo in x position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+iinclude "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that
+ * @head: pointer to the head of the list
+ * @idx: parameter number index
+ * @n: new value to save in node
+ * Return: current element within node
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+
+	listint_t *newNodoidx, *current;
+	unsigned int count = 0;
+
+	current = *head;
+
+	if (*head == NULL)
+		return (NULL);
+
+	while (current != NULL)
+	{
+
+		if (count == idx)
+		{
+			newNodoidx = malloc(sizeof(listint_t));
+
+			if (newNodoidx == NULL)
+				return (NULL);
+
+			newNodoidx->n = n;
+			newNodoidx->next = current->next;
+			current->next = newNodoidx;
+
+		}
+		count++;
+		current = current->next;
+	}
+return (newNodoidx);
+}
