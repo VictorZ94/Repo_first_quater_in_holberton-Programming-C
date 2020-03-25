74134022064b7072a8b193fd779ceea458c70b74
victorZ94 <zrvictor@outlook.com>
2020-03-25 01:16:31 +0000

    to insert new nodo in x position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,7 +34,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			current->next = newNodoidx;
 		}
 
-		if (current->next == NULL && count == (idx-1))
+		if (current->next == NULL && count == (idx - 1))
 		{
 			current->next = newNodoidx;
 			return (NULL);
