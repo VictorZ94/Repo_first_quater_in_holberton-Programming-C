ccbe178b4fae05cecb64a7b7902b1b3498ccbd63
victorZ94 <zrvictor@outlook.com>
2020-03-26 21:16:28 +0000

    to convert of binary to decimal

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - Function to convert binary to decimal.
+ * @b: parameter const char 
+ * Return: Always 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+    unsigned int decimal = 0;
+    int i = 0;
+
+    if (b == NULL)
+        return (0);
+
+    while (b[i] != '\0')
+    {
+        if (b[i] != '1' && b[i] != '0')
+            return (0);
+
+        decimal = decimal << 1;
+
+        if (b[i] == '1')
+            decimal = decimal ^ 1;
+            
+        i++;
+    }
+    return (decimal);
+}
