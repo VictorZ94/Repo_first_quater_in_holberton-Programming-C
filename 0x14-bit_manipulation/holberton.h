8f4ff67ecd0b4836c5451ab25b497ecdf6afca21
victorZ94 <zrvictor@outlook.com>
2020-03-26 23:44:21 +0000

    prototypes to functions

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,11 +2,10 @@
 #define BIT_BY_BIT_H
 #include <stdio.h>
 
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
-
-
 
 
 #endif /* prototypes BIT by BIT */
