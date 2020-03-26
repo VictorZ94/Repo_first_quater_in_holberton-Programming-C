731bb2d0143e8cb3c6b0f3049814d40587450022
victorZ94 <zrvictor@outlook.com>
2020-03-26 21:17:25 +0000

    prototype to functions

---
+++ 0x14-bit_manipulation/holberton.h
@@ -0,0 +1,27 @@
+#ifndef BIT_BY_BIT_H
+#define BIT_BY_BIT_H
+#include <stdio.h>
+
+/*int _pow_recursion(int x, int y);*/
+
+/*int _pow_recursion(int x, int y)
+{
+
+	if (y == 0)
+		return (1);
+
+	else if (y < 0)
+		return (-1);
+
+	return (_pow_recursion(x, y - 1) * x);
+}*/
+unsigned int binary_to_uint(const char *b);
+
+
+
+
+
+
+#endif /* prototypes BIT by BIT */
+
+
