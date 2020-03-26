e92621703740c2a80af3e37ce9949232c7cea8d8
victorZ94 <zrvictor@outlook.com>
2020-03-26 23:32:28 +0000

    prototype to functions

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -2,22 +2,9 @@
 #define BIT_BY_BIT_H
 #include <stdio.h>
 
-/*int _pow_recursion(int x, int y);*/
-
-/*int _pow_recursion(int x, int y)
-{
-
-	if (y == 0)
-		return (1);
-
-	else if (y < 0)
-		return (-1);
-
-	return (_pow_recursion(x, y - 1) * x);
-}*/
 unsigned int binary_to_uint(const char *b);
-
-
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 
 
