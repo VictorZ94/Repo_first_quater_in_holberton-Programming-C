9d75a6048e8a26aed8817ccc8554870aeb037c59
victorZ94 <zrvictor@outlook.com>
2020-04-01 04:38:26 +0000

    create file and to give it permmison

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,12 +22,15 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	while (text_content && text_content[i] != '\0')
+	while (text_content && text_content[i])
 	i++;
 
-	w = write(fd, text_content, i);
-	if (w < 0)
-		return (-1);
+	if (text_content)
+	{
+		w = write(fd, text_content, i);
+		if (w < 0)
+			return (-1);
+	}
 
 	c = close(fd);
 	if (c < 0)
