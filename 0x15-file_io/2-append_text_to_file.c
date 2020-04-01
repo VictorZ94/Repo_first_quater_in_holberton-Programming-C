360e8b60c70d3fe19ff435aef97ec86307636e5e
victorZ94 <zrvictor@outlook.com>
2020-04-01 04:32:57 +0000

    using oflags O_APPEND to append end of file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -10,11 +10,9 @@
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int w;
+	int w, c;
 	int i = 0;
 
-	while (text_content[i] != '\0')
-		i++;
 
 	if (filename == NULL)
 		return (-1);
@@ -26,11 +24,16 @@ int append_text_to_file(const char *filename, char *text_content)
 			if (fd  < 0)
 				return (-1);
 
+		while (text_content[i] != '\0')
+		i++;
+		
 		w = write(fd, text_content, i);
 			if (w < 0)
 				return (-1);
 
 		close(fd);
+		if (c < 0)
+				return (-1);
 
 return (1);
 }
